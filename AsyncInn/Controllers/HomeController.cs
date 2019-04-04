2d1b3f8e6d39a96cca7ad1893c9a65786f27f955
Andrew Curtis <curtis.andrewm@gmail.com>
2019-04-04 11:52:56 -0700

    Add initial file/folder build-out: Startup, HomeController, Index.cshtml, stylesheets

---
+++ AsyncInn/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Controllers
+{
+	public class HomeController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
