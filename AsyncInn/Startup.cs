f349ecadd97e8d943b8d199465dffbeaf48b063a
Andrew Curtis <curtis.andrewm@gmail.com>
2019-04-04 23:10:10 -0700

    Work on configuration properties and DbContext

--- AsyncInn/Startup.cs
+++ AsyncInn/Startup.cs
@@ -1,17 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using AsyncInn.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using AsyncInn.Data;
+using Microsoft.Extensions.Configuration;
+
 
 namespace AsyncInn
 {
 	public class Startup
 	{
+		public IConfiguration Configuration { get; }
+
+		public Startup(IConfiguration configuration)
+		{
+
+		}
+
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
