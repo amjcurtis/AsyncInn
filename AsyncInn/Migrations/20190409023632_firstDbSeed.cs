5805cdeab4579c1ebc95e5e3c5220a955585a230
Andrew Curtis <curtis.andrewm@gmail.com>
2019-04-08 19:43:45 -0700

    Add _Layout.cshtml and first successful DB seeding

---
+++ AsyncInn/Migrations/20190409023632_firstDbSeed.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AsyncInn.Migrations
+{
+    public partial class firstDbSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Amenities",
+                columns: new[] { "ID", "Name" },
+                values: new object[] { 1, "A/C" });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "ID", "City", "Name", "Phone", "State", "StreetAddress" },
+                values: new object[] { 1, "London", "Hotel 1", 1234567890, "Londonshire", "21B Baker St." });
+
+            migrationBuilder.InsertData(
+                table: "Rooms",
+                columns: new[] { "ID", "Layout", "Name" },
+                values: new object[] { 1, 0, "Presidential Suite" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Amenities",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "ID",
+                keyValue: 1);
+        }
+    }
+}
