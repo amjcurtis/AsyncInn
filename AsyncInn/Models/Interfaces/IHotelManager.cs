39d10371c24318af30a633f27842579d4283805c
Andrew Curtis <curtis.andrewm@gmail.com>
2019-04-09 12:31:48 -0700

    Add IHotelManager interface and HotelServices service (still need to write methods to get a room and all rooms)

---
+++ AsyncInn/Models/Interfaces/IHotelManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Models.Interfaces
+{
+	public interface IHotelManager
+	{
+		Task CreateHotel(Hotel hotel);
+
+		// Modify a Hotel
+		void ModifyHotel(int id);
+
+		// Delete a hotel
+		bool DeleteHotel(int id);
+
+		// Get a single hotel
+		Task<Hotel> GetHotel(int id);
+
+		// Get all the hotels
+		Task<List<Hotel>> GetHotels();
+
+		Room GetRoom(int id);
+
+		List<Room> GetAllRooms();
+	}
+}
---
+++ AsyncInn/Models/Services/HotelService.cs
@@ -0,0 +1,68 @@
+﻿using AsyncInn.Data;
+using AsyncInn.Models.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Models.Services
+{
+	public class HotelService : IHotelManager
+	{
+
+		private AsyncInnDbContext _context;
+
+		public HotelService(AsyncInnDbContext context)
+		{
+			_context = context;
+		}
+
+		public async Task CreateHotel(Hotel hotel)
+		{
+			_context.Add(hotel);
+			await _context.SaveChangesAsync();
+		}
+
+		public void ModifyHotel(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool DeleteHotel(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<Hotel> GetHotel(int id)
+		{
+			var hotel = await _context.Hotels.FindAsync(id);
+			if (hotel == null)
+			{
+				return null;
+			}
+			return hotel;
+		}
+
+		public async Task<List<Hotel>> GetHotels()
+		{
+			return await _context.Hotels.ToListAsync();
+		}
+
+		public Room GetRoom(int id)
+		{
+			//TODO Write method to get a room
+			
+			// This will likely be part of solution
+			//_context.Rooms;
+
+			return null;
+		}
+
+		public List<Room> GetAllRooms()
+		{
+			//TODO Write method to get all rooms
+
+		}
+	}
+}
